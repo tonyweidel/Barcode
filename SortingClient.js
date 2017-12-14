4752168272f195be5f909c374cc30c6ba7e39ff8
Tony Weidel <8aweidel@stpatrick.org>
2017-12-14 14:44:11 -0600

    aight we making progress

--- SortingClient.js
+++ SortingClient.js
@@ -1,7 +1,14 @@
 const Random = require("./Random.js");
 const BookFactory = require("./BookFactory.js");
 const IsbnEanAdapter = require("./IsbnEanAdapter.js");
+const Sort = require('./sort.js');
 
-function sortEan(){
-  
+
+let bf =new BookFactory()
+let list = []
+for(let x=0;x<10;x++){
+  let year = Random().discreteRangeIn(1900,2017);
+  list[x]=bf.create(year);
 }
+let isbns = [Number(EAN.sysCode)];
+let sorted = Sort.insetion(list);
